2834a8a8b5c18f6c81a58bb71986736e49e34f83
rishabh <rish6696.rs@gmail.com>
2019-08-24 22:25:55 +0530

    added unloading weight

--- routes/update.js
+++ routes/update.js
@@ -6,8 +6,15 @@ let ObjectID = require('mongodb').ObjectID;
 
 
 router.post('/',(req,res)=>{
-    console.log(req.body);
-
+    const date=(new Date()).toISOString()
+    if(req.body.truckSecurity.status){
+        console.log('updated truck security')
+        req.body.truckSecurity['date']=date
+    }
+    if(req.body.partySecurity.status){
+        console.log('updated party security')
+        req.body.partySecurity['date']=date
+    }
     MongoClient.connect(DB_url,(err,client)=>{
         if(err)
         throw err;
